c6ea92a47f8be0f29f49ef3cd58280ccbd09a03c
Cold-Egg <eleu@litespeedtech.com>
2019-05-20 13:51:51 -0400

    aws-cloudinit fix

--- Cloud-init/claunch.sh
+++ Cloud-init/claunch.sh
@@ -23,7 +23,7 @@ setupcloud(){
     ### per-instance.sh
     cat > ${CLDINITPATH}/per-instance.sh <<END 
 #!/bin/bash    
-bash <( curl -sk https://raw.githubusercontent.com/litespeedtech/ls-cloud-image/master/Cloud-init/per-instance.sh || curl -sk https://cloud.litespeed.sh/Cloud-init/per-instance.sh)
+/bin/bash <( curl -sk https://raw.githubusercontent.com/litespeedtech/ls-cloud-image/master/Cloud-init/per-instance.sh || curl -sk https://cloud.litespeed.sh/Cloud-init/per-instance.sh)
 END
     chmod 755 ${CLDINITPATH}/per-instance.sh
 }
