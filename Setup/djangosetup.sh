d3a06bcbec01ae15ddb9d930ae9a8cca318f6b6d
Cold-Egg <eleu@litespeedtech.com>
2021-02-09 10:18:32 +0800

    py 1.8

--- Setup/djangosetup.sh
+++ Setup/djangosetup.sh
@@ -2,7 +2,7 @@
 # /********************************************************************
 # LiteSpeed Django setup Script
 # @Author:   LiteSpeed Technologies, Inc. (https://www.litespeedtech.com)
-# @Copyright: (c) 2019-2020
+# @Copyright: (c) 2019-2021
 # @Version: 1.2
 # *********************************************************************/
 LSWSFD='/usr/local/lsws'
@@ -11,7 +11,7 @@ GROUP='nogroup'
 FIREWALLLIST="22 80 443"
 LSWSCONF="${LSWSFD}/conf/httpd_config.conf"
 LSWSVHCONF="${LSWSFD}/conf/vhosts/Example/vhconf.conf"
-WSGINAME='wsgi-lsapi-1.6'
+WSGINAME='wsgi-lsapi-1.8'
 PROJNAME='demo'
 PROJAPPNAME='app'
 VHDOCROOT='/usr/local/lsws/Example/html'
