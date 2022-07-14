1d6f3ea82b24de80aa822365813c290618f2f7b2
Cold-Egg <eleu@litespeedtech.com>
2022-07-14 14:41:20 +0800

    modified:   scripts/generate_ecc.sh

--- scripts/generate_ecc.sh
+++ scripts/generate_ecc.sh
@@ -1,7 +1,7 @@
 #/usr/bin/env bash
 letsencrypt_path='/etc/letsencrypt/live/'
 EPACE='        '
-
+ECC_CURV='secp384r1'
 
 check_input(){
     if [ -z "${1}" ] || [ -z "${2}" ] || [ -z "${3}" ]
@@ -19,7 +19,7 @@ echow(){
 }
 
 
-generate_ecc_ssl_certificate(){
+gen_ecc_cert(){
     w_domain=${1}
     w_email=${2}
     w_webroot=${3}
@@ -27,15 +27,14 @@ generate_ecc_ssl_certificate(){
     if [ -d $w_path ]
     then
         echo "folder exits: $w_path"
-        rm ${w_path}ecc*
-        rm ${w_path}*pem
+        rm -f ${w_path}ecc*
+        rm -f ${w_path}*pem
     else
         echo "folder not exits, create path: $w_path"
         mkdir $w_path
     fi
 
-    openssl ecparam -genkey -name secp384r1 | sudo openssl ec -out ecc.key 2>&1
-    #openssl req -new -sha256 -key ecc.key -nodes -out ecc.csr -outform pem <<csrconf
+    openssl ecparam -genkey -name ${ECC_CURV} | sudo openssl ec -out ecc.key >/dev/null 2>&1
     openssl req -new -sha256 -key ecc.key -nodes -out ecc.csr -outform pem >/dev/null 2>&1 <<csrconf
 US
 NJ
@@ -48,7 +47,7 @@ $w_domain
 .
 csrconf
     mkdir -p ${w_webroot}.well-known
-    certbot certonly --non-interactive --agree-tos --email $w_email --webroot -w $w_webroot -d $w_domain --csr ecc.csr 2>&1
+    certbot certonly --non-interactive --agree-tos --email $w_email --webroot -w $w_webroot -d $w_domain --csr ecc.csr
     mv ecc* $w_path
     mv *pem $w_path
 
@@ -70,15 +69,12 @@ help_message(){
     echo "${EPACE}${EPACE}Display help."
     echo -e "\033[1mEXAMPLE\033[0m"
     echow "generate_ecc.sh -d 'example.com' -e 'john@email.com' -w '/var/www/public_html/'"
-    echo -e "\033[1mvhost settings\033[0m"
-    echow "SSLCertificateFile /etc/letsencrypt/live/{DOMAIN}/0001_chain.pem"
-    echow "SSLCertificateKeyFile /etc/letsencrypt/live/{DOMAIN}/ecc.key"
+    echo ""
+    echow "Vhost SSL settings:"
+    echo "${EPACE}${EPACE}SSLCertificateFile /etc/letsencrypt/live/{DOMAIN}/0001_chain.pem"
+    echo "${EPACE}${EPACE}SSLCertificateKeyFile /etc/letsencrypt/live/{DOMAIN}/ecc.key"
 }
 
-
-
-# main
-#while getopts h:d:e:w: flag
 while getopts :d:e:w: flag
 do
     case "${flag}" in
@@ -97,10 +93,7 @@ then
             help_message
             ;;
         -[d] )
-            #echo domain: $domain 
-            #echo email: $email 
-            #echo webroot: $webroot
-            generate_ecc_ssl_certificate $domain $email $webroot
+            gen_ecc_cert $domain $email $webroot
             ;;
         *)
             help_message
